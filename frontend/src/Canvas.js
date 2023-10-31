5bb14fa6ebcaf01453130a7f992ce95a48353556
YaSanyaBeats <alexandr191261@yandex.ru>
2023-10-31 22:47:25 +0700

    Add Image Transformer

--- frontend/src/Canvas.js
+++ frontend/src/Canvas.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {useRef, useState, useEffect} from 'react';
-import { Stage, Layer, Rect, Transformer } from 'react-konva';
+import { Stage, Layer, Transformer, Image } from 'react-konva';
+import useImage from 'use-image';
 import Box from '@mui/material/Box';
 
 const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) => {
     const shapeRef = React.useRef();
     const trRef = React.useRef();
+
+    const [bgImage] = useImage('https://konvajs.org/assets/lion.png');
 
     React.useEffect(() => {
         if (isSelected) {
@@ -17,7 +20,8 @@ const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) => {
 
     return (
         <>
-            <Rect
+            <Image
+            image={bgImage}
             onClick={onSelect}
             onTap={onSelect}
             ref={shapeRef}
@@ -69,22 +73,20 @@ const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) => {
 };
 
 const initialRectangles = [
-{
-    x: 10,
-    y: 10,
-    width: 100,
-    height: 100,
-    fill: 'red',
-    id: 'rect1',
-},
-{
-    x: 150,
-    y: 150,
-    width: 100,
-    height: 100,
-    fill: 'green',
-    id: 'rect2',
-},
+    {
+        x: 10,
+        y: 10,
+        width: 100,
+        height: 100,
+        id: 'rect1',
+    },
+    {
+        x: 150,
+        y: 150,
+        width: 100,
+        height: 100,
+        id: 'rect2',
+    },
 ];
 
 const Canvas = () => {
@@ -94,6 +96,11 @@ const Canvas = () => {
         width: 0,
         height: 0
     })
+
+    const BGImage = () => {
+        const [bgImage] = useImage('https://konvajs.org/assets/lion.png');
+        return <Image image={bgImage} />;
+    };
 
     const checkDeselect = (e) => {
         // deselect when clicked on empty area
@@ -123,31 +130,35 @@ const Canvas = () => {
     }, [])
 
     return (
-        <Box bgcolor={'#023522'} ref={boxBounds} height={'100%'}>
+        <Box bgcolor={'#023522'} borderRadius={2} overflow='hidden' ref={boxBounds} height={'100%'}>
             <Stage
+                className='main-canvas'
                 width={dimensions.width}
                 height={dimensions.height}
                 onMouseDown={checkDeselect}
                 onTouchStart={checkDeselect}
             >
                 <Layer>
-                {rectangles.map((rect, i) => {
-                    return (
-                    <Rectangle
-                        key={i}
-                        shapeProps={rect}
-                        isSelected={rect.id === selectedId}
-                        onSelect={() => {
-                            selectShape(rect.id);
-                        }}
-                        onChange={(newAttrs) => {
-                            const rects = rectangles.slice();
-                            rects[i] = newAttrs;
-                            setRectangles(rects);
-                        }}
-                    />
-                    );
-                })}
+                    {rectangles.map((rect, i) => {
+                        return (
+                        <Rectangle
+                            key={i}
+                            shapeProps={rect}
+                            isSelected={rect.id === selectedId}
+                            onSelect={() => {
+                                selectShape(rect.id);
+                            }}
+                            onChange={(newAttrs) => {
+                                const rects = rectangles.slice();
+                                rects[i] = newAttrs;
+                                setRectangles(rects);
+                            }}
+                        />
+                        );
+                    })}
+                </Layer>
+                <Layer>
+                    
                 </Layer>
             </Stage>
         </Box>
